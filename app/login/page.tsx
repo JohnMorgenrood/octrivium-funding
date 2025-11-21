eadcc66e0ccf5b985491c1c876313b6e4d3a0b2a
Valerie <strumstudio1@gmail.com>
2025-11-21 04:29:18 +0200

    Fix Google Sign-In button to actually trigger OAuth flow

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -182,10 +182,7 @@ export default function LoginPage() {
                 variant="outline"
                 size="lg"
                 className="w-full text-sm sm:text-base border-2 dark:border-slate-700 dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-white"
-                onClick={() => {
-                  // Will be implemented with NextAuth Google provider
-                  alert('Google Sign-In will be configured with credentials later');
-                }}
+                onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
               >
                 <svg className="w-5 h-5 mr-2 sm:mr-3 shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
